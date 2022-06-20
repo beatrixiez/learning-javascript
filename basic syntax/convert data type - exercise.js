a0a00e76ac32256e40818a8b5250a693322a4303
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-06-19 21:41:44 -0300

    data type exercise

---
+++ basic syntax/convert data type - exercise.js
@@ -0,0 +1,13 @@
+let num1 = '12'
+let num2 = '19'
+
+console.log(num1 + num2)
+// concatena o valor '12' e '19' (strings)
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+const resultado = num1 + num2
+
+console.log(resultado)
+// com a conversão para o tipo número, realiza a soma de 12 e 19
