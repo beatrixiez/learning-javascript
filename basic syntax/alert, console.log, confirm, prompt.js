89d316e588b5c18a1f8525b020bbaa93c37e7716
beatriz <100150308+beatrixiez@users.noreply.github.com>
2022-06-19 21:34:31 -0300

    methods

---
+++ basic syntax/alert, console.log, confirm, prompt.js
@@ -0,0 +1,21 @@
+// ALERT 
+// exibição de uma mensagem na tela
+alert('Esta mensagem será exibida em uma janela');
+
+
+// CONSOLE.LOG
+// o método log() exibe no console a mensagem entre os parênteses
+console.log('Esta mensagem será exibida no console')
+// utilizado para testar o código, encontrar erros
+
+
+// CONFIRM
+// retorna um valor booleano
+let confirmacao = confirm('Você tem certeza?')
+// nesse caso a variavel 'confirmacao' recebe um valor true ou false
+
+
+// PROMPT
+// recebe dados inseridos pelo usuário
+let nome = prompt('Digite o seu nome:')
+// nesse caso a variavel 'nome' recebe os dados inseridos pelo usuário
