170a0cb815f414fb4cd9397d6b6c86bffedb5084
beatriz <100150308+beatrixiez@users.noreply.github.com>
2023-02-12 19:32:24 -0300

    window methods

--- basic syntax/createElement.js
+++ basic syntax/createElement.js
@@ -22,3 +22,18 @@ box.style.width = '100px' // altera a largura
 box.style.height = '100px' // altera a altura
 box.style.backgroundColor = 'blue' // altera a cor de fundo
 
+console.log(box.clientHeight) // exibe a altura do elemento
+console.log(box.clientWidth) // exibe a largura do elemento
+console.log(box.offsetTop) // retorna a distancia do do elemento até o topo da página
+console.log(box.offsetLeft) // retorna a distancia do do elemento até a margem esquerda da página
+console.log(window.innerWidth) // retorna a largura da janela, sem toolbar e scrollbar
+console.log(window.outerWidth) // retorna a largura da janela, incluindo toolbar e scrollbar
+console.log(window.pageYOffset) // retorna os pixels que um documento rolou no eixo Y
+
+const media = window.matchMedia('(max-width: 540px)') // retorna true or false com os dados da consulta
+
+if(media.matches) {
+    console.log('Tela é mobile') // se for menor que 540px
+} else {
+    console.log('Tela é desktop') // se for maior que 540px
+}
