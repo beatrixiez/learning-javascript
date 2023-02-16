353b5c4904d480dad441625226d0d27b52468ec7
beatriz <100150308+beatrixiez@users.noreply.github.com>
2023-02-16 20:40:47 -0300

    refactor: file name

--- basic syntax/forEach().js
+++
@@ -1,9 +0,0 @@
-// forEach(  ) é um método que executa uma função em cada item de um array
-// parametros em ordem: (item, index, array)
-
-const nomes = ['Beatriz', 'Luiz', 'Pedro', 'Ana']
-
-nomes.forEach((item) => {
-    item = 'Nome do aluno: ' + item
-    console.log(item)
-})
