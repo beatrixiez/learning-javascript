c69fca4830b21bc5b1f66dbcccf9c7653be7dce4
beatriz <100150308+beatrixiez@users.noreply.github.com>
2023-02-16 19:39:54 -0300

    function & arrow function

---
+++ basic syntax/function.js
@@ -0,0 +1,14 @@
+// bloco de código que pode ser reutilizado
+// para executar, é necessário "chamar" a função
+
+function somar(num1, num2) {
+    return num1 + num2
+}
+
+console.log(somar(321,895))
+
+// outra opção: arrow function
+
+const subtrair = (num1, num2) => num1 - num2
+
+console.log(subtrair(648,251))
